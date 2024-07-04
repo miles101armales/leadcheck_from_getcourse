4d05b722c29db823dce1a4c5cf83ddd75ec9b31a
miles101armales <armansk8er@gmail.com>
2024-07-04 20:41:34 +0500

    added cron function on post google api

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GoogleapisModule } from './googleapis/googleapis.module';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [GoogleapisModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    GoogleapisModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    ScheduleModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/googleapis/googleapis.controller.ts
+++ src/googleapis/googleapis.controller.ts
@@ -11,9 +11,12 @@ import {
 import { GoogleapisService } from './googleapis.service';
 import { CreateGoogleapiDto } from './dto/create-googleapi.dto';
 import { UpdateGoogleapiDto } from './dto/update-googleapi.dto';
+import { Cron } from '@nestjs/schedule';
 
 @Controller('googleapis')
 export class GoogleapisController {
+  private sales: any[][];
+  private range: string;
   constructor(private readonly googleapisService: GoogleapisService) {}
 
   @Get('send')
@@ -26,8 +29,14 @@ export class GoogleapisController {
     @Query('phone') phone: string,
   ) {
     const sale = [id, deal, dealName, name, email, phone];
+    this.range = dealName;
     console.log(sale);
-    return this.googleapisService.create(sale, dealName);
+    return this.sales.push(sale);
+  }
+
+  @Cron('0 12,18 * * *')
+  postToGoogleApi(values: any[][]) {
+    this.googleapisService.create(values, this.range);
   }
 
   @Get()
